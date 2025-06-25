0d935355a5b66050e6a8866de82175bb35c7e80c
Andre Derme <andredermee@gmail.com>
2025-06-25 20:22:42 -0300

    Create preço_produtod.java

---
+++ preço_produtod.java
@@ -0,0 +1,10 @@
+public class ativ3 {
+    public static void main(String[] args) {
+        double PreçoProduto = 29.99;
+        double Qunatidade= 3 ;
+
+        System.out.println( "o preço do produto é:");
+        System.out.println(PreçoProduto * Qunatidade);
+    }
+}
+
