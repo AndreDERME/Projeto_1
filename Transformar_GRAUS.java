b9fb6db9fcc48d00bca36f903ba6f7b5e11703d5
Andre Derme <andredermee@gmail.com>
2025-06-25 20:24:47 -0300

    Create Transformar_GRAUS.java

---
+++ Transformar_GRAUS.java
@@ -0,0 +1,9 @@
+public class ativ2 {
+    public static void main(String[] args) {
+
+        int temperatura = 30;
+        double resultado = (temperatura * 1.8) + 32;
+        System.out.println(" a transformação de graus para fahrenheit é:"  + resultado);
+
+    }
+}
