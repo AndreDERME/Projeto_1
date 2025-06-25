89b5509513ab297fd5ee7573a65501400e79b0d0
Andre Derme <andredermee@gmail.com>
2025-06-25 20:27:41 -0300

    Create media_filme.java

---
+++ media_filme.java
@@ -0,0 +1,10 @@
+public class Ativ {
+    public static void main (String [] args){
+        System.out.println("Esse é o avaliando seu filme entre amigos ");
+        System.out.println("cada um da uma nota e vemos a media desse filme");
+        System.out.println("Parasita");
+
+        double MediaFilme = (7.5 + 7.0 + 8.0) / 3;
+        System.out.println("Media Filme: " + MediaFilme);
+    }
+}
